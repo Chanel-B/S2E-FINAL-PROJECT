588d0a8e854c11c5b9070e81958ac856fe7d02b5
Chanel Boris <chanelboris@gmail.com>
2021-08-16 11:58:56 +0200

    controller package and controller class created

---
+++ product/src/main/java/com/s2e/product/controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.s2e.product.controller;
+
+import com.s2e.product.model.Product;
+import com.s2e.product.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+/**
+ * mapping all request to { "/pruducts" }
+ */
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    ProductRepository repo;
+
+    @GetMapping
+    Collection<Product> getAll(){
+        return repo.findAll();
+    }
+
+
+}
