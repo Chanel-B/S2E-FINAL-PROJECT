9bd648504f773c42389070a26d3db4f357c54896
Chanel Boris <chanelboris@gmail.com>
2021-08-16 11:53:32 +0200

    created product class

---
+++ product/src/main/java/com/s2e/product/model/Product.java
@@ -0,0 +1,28 @@
+package com.s2e.product.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * lombok viene usato qua
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private float price;
+    private int category;//int perché non vogliamo che abbia solo la primary key della category (non tutta la category)
+
+}
