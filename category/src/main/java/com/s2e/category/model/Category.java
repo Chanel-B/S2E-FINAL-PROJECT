0879c5c2174fa18231a8a9abd52f4ecc2149d095
Chanel Boris <chanelboris@gmail.com>
2021-08-16 12:33:37 +0200

    Category (model pkg and class created)

---
+++ category/src/main/java/com/s2e/category/model/Category.java
@@ -0,0 +1,25 @@
+package com.s2e.category.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * use of lombok
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+}
