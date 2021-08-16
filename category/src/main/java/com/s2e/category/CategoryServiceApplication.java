275d00a60cb9305b5d2b6e641564abd9a4e08abc
Chanel Boris <chanelboris@gmail.com>
2021-08-16 12:57:47 +0200

    added RestTamplate to the application.java
    initializing injection

--- category/src/main/java/com/s2e/category/CategoryServiceApplication.java
+++ category/src/main/java/com/s2e/category/CategoryServiceApplication.java
@@ -2,12 +2,22 @@ package com.s2e.category;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class CategoryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CategoryServiceApplication.class, args);
+		System.out.println("Category service running ...");
 	}
-
+	/**
+	 * facciamo la injection
+	 * chiamando products
+	 */
+	@Bean //
+	RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
 }
