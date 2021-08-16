3b6f35df4bf6295965abd80607449e0fdc97c665
Chanel Boris <chanelboris@gmail.com>
2021-08-16 12:40:22 +0200

    Category (Created controller )

---
+++ category/src/main/java/com/s2e/category/controller/CategoryController.java
@@ -0,0 +1,28 @@
+package com.s2e.category.controller;
+
+import com.s2e.category.model.Category;
+import com.s2e.category.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+/**
+ * use of REST annotation
+ */
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+    @Autowired
+    CategoryRepository repo;
+
+    @GetMapping
+    Collection<Category> getAll(){
+        return repo.findAll();
+    }
+
+}
