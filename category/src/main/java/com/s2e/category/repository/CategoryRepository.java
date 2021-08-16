be6418f08d8ddc8909bd85454ca150c20e8456d0
Chanel Boris <chanelboris@gmail.com>
2021-08-16 12:35:10 +0200

    Category (created repository interface)

---
+++ category/src/main/java/com/s2e/category/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.s2e.category.repository;
+
+import com.s2e.category.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+}
