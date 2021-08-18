17bff1cfaa6b20b4f806f982ccd9187d864f6c93
Chanel Boris <chanelboris@gmail.com>
2021-08-18 19:05:40 +0200

    API GATEWAY Added and is functioning now

---
+++ api-gateway/src/main/java/com/s2e/gateway/ApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.s2e.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+		System.out.println("API Gateway Running...");
+	}
+
+}
