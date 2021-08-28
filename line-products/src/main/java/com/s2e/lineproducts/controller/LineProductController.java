ab2f37014413868d2d2cae1d9d17ca8b506820c7
Chanel Boris <chanelboris@gmail.com>
2021-08-28 23:54:13 +0200

    Functioning Now

--- line-products/src/main/java/com/s2e/lineproducts/controller/LineProductController.java
+++ line-products/src/main/java/com/s2e/lineproducts/controller/LineProductController.java
@@ -15,6 +15,9 @@ import java.util.Collection;
 @RequestMapping("/lineproducts")
 public class LineProductController {
 
+    /**
+     * calling Product-Service with url
+     */
     @Value("${externalService.product.url}")
     private String productUrl;
 
